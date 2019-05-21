7eb7175a9a29d785ce009922c174c199bfa153a4
Chris Choi <chrisb.choi@gmail.com>
2019-05-21 14:55:23 +1000

    added old data

---
+++ src/app/company/company-list/olddata.ts
@@ -0,0 +1,20 @@
+export const olddata = [
+  {id: 677, name: 'ahsdh', phone: '123232', email: 'shhsdhd'},
+  {id: 675, name: 'Goobre', phone: '888', email: 'ads@goobre.com'},
+  {id: 676, name: 'Tesco', phone: '044', email: 'tesco@tesco.co'},
+  {
+    id: 573,
+    name: 'Master Company',
+    phone: '16353453463663',
+    email: 'master@company.com',
+  },
+  {id: 671, name: 'Evil Corp 2', phone: '999', email: '2@evilcorp.co'},
+  {id: 660, name: '123', phone: '123', email: '123'},
+  {id: 661, name: 'new company', phone: '', email: ''},
+  {
+    id: 665,
+    name: 'Company B',
+    phone: '0123456789',
+    email: 'companyB@email.com',
+  },
+];
