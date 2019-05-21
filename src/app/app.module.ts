f36ac846bdacc79bd76589057b30e4151d2477c6
Chris Choi <chrisb.choi@gmail.com>
2019-05-21 11:29:12 +1000

    company-list added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { CompanyListComponent } from './company/company-list/company-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CompanyListComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
