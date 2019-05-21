31b37bc4a10ac7db2b787ecef20ac6a379c8562a
Chris Choi <chrisb.choi@gmail.com>
2019-05-21 12:29:20 +1000

    added ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { CompanyListComponent } from './company/company-list/company-list.component';
+import {CompanyListComponent} from './company/company-list/company-list.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, CompanyListComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
