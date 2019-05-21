c0bf9f1ddaf5d625897a8504b0ccf0daf17034f3
Chris Choi <chrisb.choi@gmail.com>
2019-05-21 17:32:23 +1000

    enabled router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CompanyListComponent} from './company/company-list/company-list.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {HttpClientModule} from '@angular/common/http';
+import {CompanyEditComponent} from './company/company-edit/company-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, CompanyListComponent],
+  declarations: [AppComponent, CompanyListComponent, CompanyEditComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
