1172cbebfad3b2589ae429c30eda75598c5a70eb
Chris Choi <chrisb.choi@gmail.com>
2019-05-21 15:32:22 +1000

    async pipe

--- src/app/company/company-list/company-list.component.ts
+++ src/app/company/company-list/company-list.component.ts
@@ -1,33 +1,19 @@
-import {CompanyService} from './../company.service';
 import {Component, OnInit, OnDestroy} from '@angular/core';
+import {CompanyService} from '../company.service';
+import {Observable} from 'rxjs';
 import {Company} from './company';
-import {tap, takeWhile} from 'rxjs/operators';
-import {Subscription} from 'rxjs';
+
 @Component({
   selector: 'fbc-company-list',
   templateUrl: './company-list.component.html',
   styleUrls: ['./company-list.component.scss'],
 })
-export class CompanyListComponent implements OnInit, OnDestroy {
+export class CompanyListComponent implements OnInit {
   constructor(private companySvc: CompanyService) {}
-  componentExists = true;
 
-  companies: Company[];
-  ngOnDestroy(): void {
-    this.componentExists = false;
-  }
+  companies$: Observable<Company[]>;
 
   ngOnInit() {
-    this.companySvc
-      .getCompanies()
-      .pipe(
-        takeWhile(c => this.componentExists),
-        tap(c => console.log(`Tab got ${c.length} companies`)),
-      )
-      .subscribe(
-        next => (this.companies = next),
-        error => console.error('ERROR', error),
-        () => console.log('COMPLETE'),
-      );
+    this.companies$ = this.companySvc.getCompanies();
   }
 }
