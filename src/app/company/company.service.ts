34a5ab1d4d6d98f27e8fb75d404bd660efde9196
Chris Choi <chrisb.choi@gmail.com>
2019-05-21 15:54:53 +1000

    added errir handling

--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -3,15 +3,22 @@ import {Company} from './company-list/company';
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-
+import {catchError} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
 export class CompanyService {
   constructor(private httpClient: HttpClient) {}
-  API_BASE = 'http://firebootcamp-crm-api.azurewebsites.net/api';
-
+  // API_BASE = 'http://firebootcamp-crm-api.azurewebsites.net/api';
+  API_BASE = environment.API_BASE;
   getCompanies(): Observable<Company[]> {
-    return this.httpClient.get<Company[]>(`${this.API_BASE}/company`);
+    return this.httpClient
+      .get<Company[]>(`${this.API_BASE}/company`)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  private errorHandler(error: Error): Observable<Company[]> {
+    console.error('implement custom errort handler here', error);
+    return new Observable<Company[]>();
   }
 }
