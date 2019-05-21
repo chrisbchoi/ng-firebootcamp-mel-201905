ae46b2d17808dbc02d156b5c2fadc074438ed8db
Chris Choi <chrisb.choi@gmail.com>
2019-05-21 12:06:58 +1000

    added table

--- src/app/company/company-list/company-list.component.ts
+++ src/app/company/company-list/company-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Company} from './company';
 
 @Component({
   selector: 'fbc-company-list',
   templateUrl: './company-list.component.html',
-  styleUrls: ['./company-list.component.scss']
+  styleUrls: ['./company-list.component.scss'],
 })
 export class CompanyListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  companies: Company[] = [
+    {
+      name: 'SSW',
+      phone: 12345,
+      email: 'info@ssw.com',
+    },
+    {
+      name: 'Microsoft',
+      phone: 12344,
+      email: 'info@microsoft.com',
+    },
+    {
+      name: 'Google',
+      phone: 122145,
+      email: 'info@google.com',
+    },
+  ];
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
