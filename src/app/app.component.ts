62f48f0ac32ff86a324037f41947ecae1a70b986
Chris Choi <chrisb.choi@gmail.com>
2019-05-21 11:10:18 +1000

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'fbc-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'firebootcamp-crm';
