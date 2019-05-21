b3df6a0edfe85784fd5fcd6c7331ace8ef5842d8
Chris Choi <chrisb.choi@gmail.com>
2019-05-21 15:16:56 +1000

    manual subscription

--- src/app/company/company-list/company-list.component.ts
+++ src/app/company/company-list/company-list.component.ts
@@ -1,21 +1,28 @@
 import {CompanyService} from './../company.service';
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {Company} from './company';
 import {tap} from 'rxjs/operators';
+import {Subscription} from 'rxjs';
 @Component({
   selector: 'fbc-company-list',
   templateUrl: './company-list.component.html',
   styleUrls: ['./company-list.component.scss'],
 })
-export class CompanyListComponent implements OnInit {
+export class CompanyListComponent implements OnInit, OnDestroy {
   constructor(private companySvc: CompanyService) {}
+  sub: Subscription;
 
   companies: Company[];
+  ngOnDestroy(): void {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
 
   ngOnInit() {
-    this.companySvc
+    this.sub = this.companySvc
       .getCompanies()
-      .pipe(tap(c => 'Tab got ${c.length} companies'))
+      .pipe(tap(c => console.log(`Tab got ${c.length} companies`)))
       .subscribe(
         next => (this.companies = next),
         error => console.error('ERROR', error),
