6be1a2559e4cdb7cdb63cee68c81a5ff9de67688
Chris Choi <chrisb.choi@gmail.com>
2019-05-22 12:51:46 +1000

    update initial state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import {Component} from '@angular/core';
+
+import { CompanyService } from './company/company.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'fbc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'firebootcamp-crm';
+  companyCount$: Observable<number>;
+
+  constructor(private companyService: CompanyService) {
+
+
+  }
+  ngOnInit(): void {
+    this.companyCount$ = this.companyService.getCompanies()
+      .pipe(map(list => list.length));
+
+
+  }
 }
