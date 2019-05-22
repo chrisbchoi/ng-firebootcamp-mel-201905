b7b881ab3a1c95cd6543824b62c2475ebcf06c09
Chris Choi <chrisb.choi@gmail.com>
2019-05-22 13:44:21 +1000

    enabled back prettier

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -1,22 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { CompanyService } from '../company.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Company } from '../company-list/company';
-
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {CompanyService} from '../company.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Company} from '../company-list/company';
 
 @Component({
   selector: 'fbc-company-edit',
   templateUrl: './company-edit.component.html',
-  styleUrls: ['./company-edit.component.scss']
+  styleUrls: ['./company-edit.component.scss'],
 })
 export class CompanyEditComponent implements OnInit {
-
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private companyService: CompanyService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder,
+  ) {}
 
   company: Company;
   isNewCompany: boolean;
@@ -24,13 +23,14 @@ export class CompanyEditComponent implements OnInit {
   companyForm: FormGroup;
 
   ngOnInit() {
-    this.isNewCompany = !this.activatedRoute.snapshot.params['id'];
+    this.isNewCompany = !this.activatedRoute.snapshot.params.id;
     this.buildForm();
 
     if (!this.isNewCompany) {
-      this.companyId = this.activatedRoute.snapshot.params['id'];
+      this.companyId = this.activatedRoute.snapshot.params.id;
 
-      this.companyService.getCompany(this.companyId)
+      this.companyService
+        .getCompany(this.companyId)
         .subscribe(company => this.companyForm.patchValue(company));
     }
   }
@@ -39,20 +39,21 @@ export class CompanyEditComponent implements OnInit {
     this.companyForm = this.fb.group({
       name: ['', Validators.required],
       phone: [''],
-      email: ['']
+      email: [''],
     });
   }
 
   saveCompany() {
     console.log('SAVING FORM', this.companyForm);
     if (this.isNewCompany) {
-      this.companyService.addCompany(this.companyForm.value)
+      this.companyService
+        .addCompany(this.companyForm.value)
         .subscribe(c => this.router.navigateByUrl('/company/list'));
     } else {
-      const company: Company = { ... this.companyForm.value, id: this.companyId };
-      this.companyService.updateCompany(company)
+      const company: Company = {...this.companyForm.value, id: this.companyId};
+      this.companyService
+        .updateCompany(company)
         .subscribe(c => this.router.navigateByUrl('/company/list'));
     }
   }
-
 }
