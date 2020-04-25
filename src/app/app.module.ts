9975dd9d7d9c894e6c88ca5c9fee5e383c723039
Chris Choi <chrisb.choi@gmail.com>
2020-04-25 20:00:04 +1000

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {CompanyListComponent} from './company/company-list/company-list.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {HttpClientModule} from '@angular/common/http';
-import {CompanyEditComponent} from './company/company-edit/company-edit.component';
-import { CompanyTableComponent } from './company/company-table/company-table.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CompanyListComponent } from "./company/company-list/company-list.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { HttpClientModule } from "@angular/common/http";
+import { CompanyEditComponent } from "./company/company-edit/company-edit.component";
+import { CompanyTableComponent } from "./company/company-table/company-table.component";
 
 @NgModule({
-  declarations: [AppComponent, CompanyListComponent, CompanyEditComponent, CompanyTableComponent],
+  declarations: [
+    AppComponent,
+    CompanyListComponent,
+    CompanyEditComponent,
+    CompanyTableComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    NgbModule.forRoot(),
+    //NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
